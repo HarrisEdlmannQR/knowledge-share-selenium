003a7d6d01e4046ba73f309db88dd039b8cf9e62
HarrisEdlmannQR <harris.edlmann@quickrelease.co.uk>
2025-06-13 10:23:11 +0100

    feat: add my solution

--- main.py
+++ main.py
@@ -24,4 +24,49 @@ driver.get("https://qrthehub.co.uk/")
 
 """your code here"""
 
-# driver.quit() # even with 'detach'=True this will close the window
+# Click SSO button
+sso_button = driver.find_element(by=By.ID, value="sso")
+sso_button.click()
+
+# Input email and click 'Next'
+email = "harris.edlmann@quickrelease.co.uk"
+
+email_input = driver.find_element(by=By.ID, value="i0116")
+email_input.send_keys(email)
+
+email_next_button = driver.find_element(by=By.ID, value="idSIButton9")
+email_next_button.click()
+
+time.sleep(10) # discuss implicitly_wait
+
+# Input password and click 'Sign in'
+password = pwinput.pwinput(prompt="Enter Password: ", mask="*")
+print(f"Password Received!")
+
+password_input = driver.find_element(by=By.ID, value="i0118")
+password_input.send_keys(password)
+
+password_sign_in_button = driver.find_element(by=By.ID, value="idSIButton9")
+password_sign_in_button.click()
+
+# Input Authentication code and click 'Verify'
+auth_code = pwinput.pwinput(prompt="Enter Authentication code: ", mask="*")
+print(f"Authenticator Code Received!")
+
+authentication_input = driver.find_element(by=By.ID, value="idTxtBx_SAOTCC_OTC")
+authentication_input.send_keys(auth_code)
+
+authentication_verify_button = driver.find_element(by=By.ID, value="idSubmit_SAOTCC_Continue")
+authentication_verify_button.click()
+
+time.sleep(2)
+
+# CLick 'Yes' to stay signed in
+stay_sign_in_button = driver.find_element(by=By.ID, value="idSIButton9")
+stay_sign_in_button.click()
+
+# Get greeting message from homepage
+greeting_message = driver.find_element(by=By.XPATH, value="//div[@class='col-lg-8']//div//div//div//div//h3")
+print(f"Greeting message is, '{greeting_message.text.rsplit(' ', 1)[0]}'")
+
+driver.quit() # even with 'detach'=True this will close the window
